1cd0a7c2d6a47238fee37df525d2a8c0eb3cd301
Zipeerix <ziperix@icloud.com>
2024-07-10 18:14:45 +0200

    CAS-92: Attempt

--- tests/algorithms/AlphaBlendingTests.cpp
+++ tests/algorithms/AlphaBlendingTests.cpp
@@ -27,7 +27,7 @@ TEST(algorithms, alpha_blending_one_layer) {
       capy::Pixel{128, 128, 128, 255},
   };
 
-  capy::algorithms::AlphaBlender blender([&](int, int, int layer) { return layers.at(layer); });
+  const auto blender = capy::algorithms::AlphaBlender([&](int, int, int layer) { return layers.at(layer); });
 
   const auto blendResult = blender.blend(0, 0, layers.size());
   ASSERT_EQ(blendResult, QColor(128, 128, 128, 255));
@@ -36,7 +36,7 @@ TEST(algorithms, alpha_blending_one_layer) {
 TEST(algorithms, alpha_blending_two_layers) {
   std::vector<capy::Pixel> layers = {capy::Pixel{128, 128, 128, 255}, capy::Pixel{0, 0, 0, 128}};
 
-  capy::algorithms::AlphaBlender blender([&](int, int, int layer) { return layers.at(layer); });
+  const auto blender = capy::algorithms::AlphaBlender([&](int, int, int layer) { return layers.at(layer); });
 
   const auto blendResult = blender.blend(0, 0, layers.size());
   ASSERT_EQ(blendResult, QColor(64, 64, 64, 255));
@@ -45,7 +45,7 @@ TEST(algorithms, alpha_blending_two_layers) {
 TEST(algorithms, alpha_blending_two_layers_transparent_base) {
   std::vector<capy::Pixel> layers = {capy::Pixel{128, 128, 128, 0}, capy::Pixel{0, 0, 0, 128}};
 
-  capy::algorithms::AlphaBlender blender([&](int, int, int layer) { return layers.at(layer); });
+  const auto blender = capy::algorithms::AlphaBlender([&](int, int, int layer) { return layers.at(layer); });
 
   const auto blendResult = blender.blend(0, 0, layers.size());
   ASSERT_EQ(blendResult, QColor(64, 64, 64, 255));
@@ -58,7 +58,7 @@ TEST(algorithms, alpha_blending_n_layers) {
       capy::Pixel{191, 155, 21, 180},
   };
 
-  capy::algorithms::AlphaBlender blender([&](int, int, int layer) { return layers.at(layer); });
+  const auto blender = capy::algorithms::AlphaBlender([&](int, int, int layer) { return layers.at(layer); });
 
   const auto blendResult = blender.blend(0, 0, layers.size());
   ASSERT_EQ(blendResult, QColor(158, 127, 27, 254));
